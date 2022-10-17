19c90ebc1d4b7d6e212247baa153c298cc769f22
Jun <jun55tsuno@gmail.com>
2022-10-16 20:27:30 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
 //import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 // const items = [
 //     {
@@ -18,31 +19,11 @@ import Dropdown from "./components/Dropdown";
 //     },
 // ]
 
-const options = [
-    {
-        label: 'The color Red',
-        value: 'red'
-    },
-    {
-        label: 'The color Green',
-        value: 'green'
-    },
-    {
-        label: 'A Shade of Blue',
-        value: 'blue'
-    }
-]
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0]);
-
     return (
         <div>
-            <Dropdown
-                options={options}
-                selected={selected}
-                onSelectedChange={setSelected}
-            />
+            <Translate />
         </div>
     );
 };
