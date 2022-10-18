be22b6c597806617a1cf0b6addac4c0716e903fb
Jun <jun55tsuno@gmail.com>
2022-10-17 23:13:47 -0700

    updated

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
 
 const options = [
     {
@@ -17,18 +19,27 @@ const options = [
 ];
 
 const Translate = () => {
-    const [language, setLanguage] = useState(options[0])
+    const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e)=> setText(e.target.value)} />
+                </div>
+            </div>
             <Dropdown
                 label="Select a Language"
                 selected={language}
                 onSelectedChange={setLanguage}
                 options={options} />
+            <hr />
+            <h3 className="ui header">output</h3>
+            <Convert text={text} language={language} />
         </div>
     )
-
 };
 
 export default Translate;
